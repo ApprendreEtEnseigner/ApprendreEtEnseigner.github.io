774fd255aa75aa8111ae3edd8f0aab91e597d2a6
sow <yeroscorpion149@gmail.com>
2024-08-10 17:51:12 +0100

    add feature navlinks line indicator and add contents on articles topic

--- a-et-e-v24/index.js
+++ a-et-e-v24/index.js
@@ -1,23 +1,32 @@
-const nav = document.querySelector('nav');
-const mobileNav = document.querySelector('nav.mobile-nav');
-const menuIcon = document.querySelector('.menu-icon');
-const closeIcon = document.querySelector('.mobile-menu-container .close-icon');
-const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+// const links = document.querySelectorAll('nav ul li .feature-clic')
+const nav = document.querySelector("nav");
+const activePage = window.location.pathname;
+const links = document.querySelectorAll(".nav-links ul .link-underline a");
+const mobileNav = document.querySelector("nav.mobile-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 60) {
-        nav.classList.add('scrolled');
-        mobileNav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-        mobileNav.classList.remove('scrolled');
-    }
+links.forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
 });
 
-menuIcon.addEventListener('click', () => {
-    mobileMenuContainer.classList.add('active');
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 60) {
+    nav.classList.add("scrolled");
+    mobileNav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+    mobileNav.classList.remove("scrolled");
+  }
 });
 
-closeIcon.addEventListener('click', () => {
-    mobileMenuContainer.classList.remove('active');
+menuIcon.addEventListener("click", () => {
+  mobileMenuContainer.classList.add("active");
+});
+
+closeIcon.addEventListener("click", () => {
+  mobileMenuContainer.classList.remove("active");
 });
--- a-et-e-v24/nav-pages/navigation.js
+++ a-et-e-v24/nav-pages/navigation.js
@@ -1,80 +1,82 @@
+let buttons = document.querySelectorAll(".btn.dark");
+let buttonsDefis = document.querySelectorAll(".btn.dark.defis");
 
-let buttons = document.querySelectorAll('.btn.dark');
-let buttonsDefis = document.querySelectorAll('.btn.dark.defis');
+let titles = document.querySelectorAll(".title-theme h2");
+let titlesDefis = document.querySelectorAll(".title-module h2");
 
-let titles = document.querySelectorAll('.title-theme h2');
-let titlesDefis = document.querySelectorAll('.title-module h2');
-
-let contents = document.querySelectorAll('.content-articles, .content-videos, .content-slides, .content-podcasts');
-let contentsDefis = document.querySelectorAll('.content-penseealgorithmique, .content-python, .content-linux, .content-langager, .content-basededonnees');
+let contents = document.querySelectorAll(
+  ".content-articles, .content-videos, .content-slides, .content-podcasts"
+);
+let contentsDefis = document.querySelectorAll(
+  ".content-penseealgorithmique, .content-python, .content-linux, .content-langager, .content-basededonnees"
+);
 
 titles.forEach((title) => {
-    title.style.display = 'none';
+  title.style.display = "none";
 });
 
 titlesDefis.forEach((title) => {
-    title.style.display = 'none';
+  title.style.display = "none";
 });
 
 contents.forEach((content) => {
-    content.style.display = 'none';
+  content.style.display = "none";
 });
 
 contentsDefis.forEach((content) => {
-    content.style.display = 'none';
+  content.style.display = "none";
 });
 
 buttons.forEach((button) => {
-    button.addEventListener('click', handleClick);
+  button.addEventListener("click", handleClick);
 });
 
 buttonsDefis.forEach((button) => {
-    button.addEventListener('click', handleDefisClick);
+  button.addEventListener("click", handleDefisClick);
 });
 
 if (titles.length > 0) {
-    titles[0].style.display = 'block';
+  titles[0].style.display = "block";
 }
 if (contents.length > 0) {
-    contents[0].style.display = 'block';
+  contents[0].style.display = "block";
 }
 
 if (titlesDefis.length > 0) {
-    titlesDefis[0].style.display = 'block';
+  titlesDefis[0].style.display = "block";
 }
 if (contentsDefis.length > 0) {
-    contentsDefis[0].style.display = 'block';
+  contentsDefis[0].style.display = "block";
 }
-// console.log('contentsDefis:', contentsDefis)
 
 function handleClick(event) {
+  let id = event.target.getAttribute("href").slice(1);
 
-    let id = event.target.getAttribute('href').slice(1);
+  titles.forEach((title) => {
+    title.style.display = "none";
+  });
 
-    titles.forEach((title) => {
-        title.style.display = 'none';
-    });
+  contents.forEach((content) => {
+    content.style.display = "none";
+  });
 
-    contents.forEach((content) => {
-        content.style.display = 'none';
-    });
-
-    document.getElementById(id).style.display = 'block';
-    document.querySelector('.content-' + id).style.display = 'block';
-};
+  document.getElementById(id).style.display = "block";
+  document.querySelector(".content-" + id).style.display = "block";
+}
 
 function handleDefisClick(event) {
+  let id = event.target.getAttribute("href").slice(1);
 
-    let id = event.target.getAttribute('href').slice(1);
+  titlesDefis.forEach((title) => {
+    title.style.display = "none";
+  });
 
-    titlesDefis.forEach((title) => {
-        title.style.display = 'none';
-    });
+  contentsDefis.forEach((content) => {
+    content.style.display = "none";
+  });
 
-    contentsDefis.forEach((content) => {
-        content.style.display = 'none';
-    });
+  document.getElementById(id).style.display = "block";
+  document.querySelector(".content-" + id).style.display = "block";
+}
 
-    document.getElementById(id).style.display = 'block';
-    document.querySelector('.content-' + id).style.display = 'block';
-};
+// ------------------------------
